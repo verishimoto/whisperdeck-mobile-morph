598218354ea4612190de5f769c7ffe2850019bb6
verishimoto <vg.contato@gmail.com>
2025-10-24 08:13:52 -0300

    Fix import statement for cards.js in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import './assets/cards.js';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";import './assets/cards.js';
+import "./index.css";
 
 
 createRoot(document.getElementById("root")!).render(<App />);
